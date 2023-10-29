3976f9493925804cda3242cdd42bdc93def01ca2
Molodoy3 <artem.nikonov.2005@bk.ru>
2023-10-29 13:25:27 +0500

    заказ 5 - измененние title, добавление discription, исправление ссылок, html ошибок

--- src/js/modules/formValidate.js
+++ src/js/modules/formValidate.js
@@ -1,100 +1,106 @@
 export function formValidate() {
-    const forms = document.forms;
-    if (forms.length) {
-        let form = forms[0];
-        for (let index = 0; index < forms.length; index++) {
-            form = forms[index];
-            form.addEventListener('submit', formSend);
-        }
-        function formSend(e) {
-            e.preventDefault();
-            let errors = formValidate(form);
-            if (errors === 0) {
-                /* alert('письмо отправлено');
-                form.reset();
-                const errorElements = form.querySelectorAll('.error-element');
-                if(errorElements.length){
-                    errorElements.forEach(item => {
-                        item.remove();
-                    });
-                } */
-            } else {
-                form.classList.add('error');
-            }
-        }
-        function formValidate(form) {
-            let errors = 0;
-            const formReq = form.querySelectorAll('.req');
+    
 
-            if (formReq.length) {
-                for (let index = 0; index < formReq.length; index++) {
-                    const input = formReq[index];
-                    formRemoveError(input);
-                    if (input.classList.contains('contact')) {
-                        if (emailTest(input) && phoneTest(input)) {
-                            formAddError(input);
-                            errors++;
-                        }
-                    } else if (input.classList.contains('select')) {
-                        if (input.options[input.selectedIndex].disabled) {
-                            formAddError(input);
-                            errors++;
-                        }
-                    }
-                    else if (input.dataset.validateMin) {
-                        const validateMinValue = input.dataset.validateMin;
-                        if (input.value.length < validateMinValue) {
-                            formAddError(input);
-                            errors++;
-                            if (input.dataset.textError) {
-                                const textError = input.dataset.textError;
-                                const errorElementOld = input.parentElement.querySelector('.error-element');
-                                if (errorElementOld) {
-                                    errorElementOld.remove();
-                                }
-                                input.insertAdjacentHTML(
-                                    'afterend',
-                                    `<div class="error-element">${textError}</div>`
-                                )
+    var A = document.querySelectorAll("[type=submit]");
+    const e = document.forms;
+    if (e.length) {
+        
+        A.forEach((function(e) {
+            return e.addEventListener("click", s)
+        }));
+        //for (let l = 0; l < e.length; l++) {
+        //	t = e[l];
+        //	t.addEventListener("submit", s);
+        //}
+
+        function s(e) {
+            
+            e.preventDefault();
+            let t = this.closest("form");
+            console.log(t);
+            //send(t);
+            console.log(i(t)),
+            0 === i(t) ? send(t) : t.classList.add("error")
+        }
+
+
+        function send(t) {
+            console.log(t);
+            var formData = new FormData(t),
+                n = new XMLHttpRequest,
+                o = "/send.php";
+            n.open("POST", o, true),
+            n.send(formData),
+            
+            n.onload = function () {
+                if(n.responseText == 'ok') {
+                    console.log('sucsess'),
+                    console.log(n.responseText),
+                    t.innerHTML = "<p style='padding:100px 10px;text-align:center'>Заказ успешно отправлен!</p>"
+                    //m(e, "error")
+                    //l.classList.add("d-none"),
+                   //b.classList.remove("d-none")
+                }
+                else{
+                    //m(e, "accepted"),
+                    console.log('error'),
+                    console.log(n.responseText),
+                    t.innerHTML = "<p style='padding:100px 10px;text-align:center'>"+n.responseText+"</p>"
+                   
+                    //setTimeout((function() {
+                    //    window.open(n.responseText, '_top');
+                    //}), 2500)
+                }
+            }
+
+        }
+
+        function i(e) {
+            let t = 0;
+            const s = e.querySelectorAll(".req");
+            if (s.length) {
+                for (let e = 0; e < s.length; e++) {
+                    const i = s[e];
+                    if (r(i), i.classList.contains("contact")) a(i) && o(i) && (n(i), t++);
+                    else if (i.classList.contains("select")) i.options[i.selectedIndex].disabled && (n(i), t++);
+                    else if (i.dataset.validateMin) {
+                        const e = i.dataset.validateMin;
+                        if (i.value.length < e) {
+                            if (n(i), t++, i.dataset.textError) {
+                                const e = i.dataset.textError,
+                                    t = i.parentElement.querySelector(".error-element");
+                                t && t.remove(), i.insertAdjacentHTML("afterend", `<div class="error-element">${e}</div>`)
                             }
                         } else {
-                            const errorElementOld = input.parentElement.querySelector('.error-element');
-                            if (errorElementOld) {
-                                errorElementOld.remove();
-                            }
+                            const e = i.parentElement.querySelector(".error-element");
+                            e && e.remove()
                         }
-                    } else if (input.value === '') {
-                        formAddError(input);
-                        errors++;
-                    }
+                    } else "" === i.value && (n(i), t++)
                 }
-                return (errors);
+                return t
             }
+        }
 
+        function r(e) {
+            e.classList.remove("error"), e.parentElement.classList.remove("error")
         }
-        function formRemoveError(input) {
-            input.classList.remove('error');
-            input.parentElement.classList.remove('error');
+
+        function n(e) {
+            e.classList.add("error"), e.parentElement.classList.add("error")
         }
-        function formAddError(input) {
-            input.classList.add('error');
-            input.parentElement.classList.add('error');
+
+        function a(e) {
+            return !/^\w+([\.*]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(e.value)
         }
-        function emailTest(input) {
-            return !/^\w+([\.*]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+
+        function o(e) {
+            const t = e.value.replace(/\D/g, "");
+            return !/^\d{10,15}$/.test(t)
         }
-        function phoneTest(input) {
-            // Удаляем все символы, кроме цифр
-            const phoneNumber = input.value.replace(/\D/g, '');
-            
-            // Проверяем, что номер содержит от 10 до 15 цифр
-            if (/^\d{10,15}$/.test(phoneNumber)) {
-              return false;
-            } else {
-              return true;
-            }
-          }
     }
+
+
+    
 }
 
 
